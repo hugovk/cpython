40f6cc15d2fe7d2721bb0585a94d0db1ea8d0385
Hugo <hugovk@users.noreply.github.com>
2020-01-14 23:42:38 +0200

    Use sys.version_info instead of sys.version, its format an implementation detail of CPython

--- Lib/sysconfig.py
+++ Lib/sysconfig.py
@@ -84,9 +84,7 @@ _INSTALL_SCHEMES = {
 _SCHEME_KEYS = ('stdlib', 'platstdlib', 'purelib', 'platlib', 'include',
                 'scripts', 'data')
 
- # FIXME don't rely on sys.version here, its format is an implementation detail
- # of CPython, use sys.version_info or sys.hexversion
-_PY_VERSION = sys.version.split()[0]
+_PY_VERSION = '%d.%d.%d' % sys.version_info[:3]
 _PY_VERSION_SHORT = '%d.%d' % sys.version_info[:2]
 _PY_VERSION_SHORT_NO_DOT = '%d%d' % sys.version_info[:2]
 _PREFIX = os.path.normpath(sys.prefix)
