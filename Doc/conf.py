c69d74f257c19b511e555201f881b88346d8961a
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-09-27 12:06:53 -0700

    WIP PyData Sphinx Theme

--- Doc/conf.py
+++ Doc/conf.py
@@ -286,10 +286,10 @@ html_theme_options = {
     # 'root_include_title': False,  # We use the version switcher instead.
 }
 
-if os.getenv("READTHEDOCS"):
-    html_theme_options["hosted_on"] = (
-        '<a href="https://about.readthedocs.com/">Read the Docs</a>'
-    )
+# if os.getenv("READTHEDOCS"):
+#     html_theme_options["hosted_on"] = (
+#         '<a href="https://about.readthedocs.com/">Read the Docs</a>'
+#     )
 
 # Override stylesheet fingerprinting for Windows CHM htmlhelp to fix GH-91207
 # https://github.com/python/cpython/issues/91207
