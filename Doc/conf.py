2237a62936339e6d5688bd4812722700bd9c38e9
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-10-24 12:26:51 +0300

    Add html_favicon

--- Doc/conf.py
+++ Doc/conf.py
@@ -309,6 +309,7 @@ html_theme_options = {
     },
     "navbar_align": "left",
 }
+html_favicon = "https://devguide.python.org/_static/python-logo.svg"
 html_logo = "https://devguide.python.org/_static/python-logo.svg"
 
 # if os.getenv("READTHEDOCS"):
